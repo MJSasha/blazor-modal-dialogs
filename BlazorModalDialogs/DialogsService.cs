8cdad6399f06cb7b6887f356c6db9a95e06c4880
MJSasha <sasha.ushankov@gmail.com>
2023-06-25 11:26:12 +0300

    Update lib version

--- BlazorModalDialogs/DialogsService.cs
+++ BlazorModalDialogs/DialogsService.cs
@@ -4,25 +4,25 @@ namespace BlazorModalDialogs
 {
     public class DialogsService
     {
-        private List<object> Dialogs = new List<object>();
+        private List<object> dialogs = new();
 
-        public void RegisterDialog<TDialog>(TDialog dialog)
+        internal void RegisterDialog<TDialog>(TDialog dialog)
         {
-            var registeredDialog = Dialogs.Where(d => d.GetType() == typeof(TDialog)).SingleOrDefault();
-            if (registeredDialog != null) Dialogs.Remove(registeredDialog);
-            Dialogs.Add(dialog);
+            var registeredDialog = dialogs.SingleOrDefault(d => d.GetType() == typeof(TDialog));
+            if (registeredDialog != null) throw new KeyNotFoundException($"Dialog ${typeof(TDialog)} already exist");
+            dialogs.Add(dialog);
         }
 
         public Task<TResult> Show<TDialog, TParams, TResult>(TParams parameters) where TDialog : Dialog<TParams, TResult>
         {
-            var dialog = Dialogs.Where(d => d.GetType() == typeof(TDialog)).SingleOrDefault();
+            var dialog = dialogs.SingleOrDefault(d => d.GetType() == typeof(TDialog));
             if (dialog != null) return ((Dialog<TParams, TResult>)dialog).Show(parameters);
             else throw new KeyNotFoundException($"Dialog ${typeof(TDialog)} not found");
         }
 
-        public void Close<TDialog, TParams, TResult>() where TDialog : Dialog<TParams, TResult>
+        public void Hide<TDialog, TParams, TResult>() where TDialog : Dialog<TParams, TResult>
         {
-            (Dialogs.SingleOrDefault(d => d.GetType() == typeof(TDialog)) as TDialog).Hide();
+            (dialogs.SingleOrDefault(d => d.GetType() == typeof(TDialog)) as TDialog).Hide();
         }
     }
 }
